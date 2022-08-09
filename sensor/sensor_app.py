5a5e81dbd08e6037b72afff87e0e3df289466a24
Deng <jujis008@163.com>
2022-08-09 22:34:05 +0800

    fmt

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -12,16 +12,10 @@ print("Sensor Data App")
 
 time_fmt = '%m/%d/%y'
 
-##############################
-
-# Module 1 code here:
-
-# Module 2 code here:
-
 data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 
-# Module 3 code here:
+
 house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area('id', test_area)
@@ -31,6 +25,15 @@ print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 test_date = datetime.strptime('5/9/20', time_fmt)
 recs = house_info.get_data_by_date('id', rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime(time_fmt), len(recs)))
+
+##############################
+
+# Module 1 code here:
+
+# Module 2 code here:
+
+# Module 3 code here:
+
 # Module 4 code here:
 
 # Module 5 code here:
