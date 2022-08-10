0fc787a51c81e1ed538e41e6216bdc5a867be790
Deng <jujis008@163.com>
2022-08-10 10:21:48 +0800

    for test case

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,7 +1,7 @@
-from datetime import datetime, date
+from datetime import date
 
 class HouseInfo:
-  def __init__(self, data):
+  def __init__(self, data=[]):
     self.data = data
 
   def get_data_by_area(self, field, rec_area=0):
@@ -13,10 +13,10 @@ class HouseInfo:
         field_data.append(record[field])
     return field_data
 
-  def get_data_by_date(self, field, rec_date=datetime.now()):
+  def get_data_by_date(self, field, rec_date=date.today()):
     field_data = []
     for record in self.data:
-      if datetime.strftime(rec_date, '%m/%d/%y') == record['date']:
+      if rec_date.strftime('%m/%d/%y') == record['date']:
         field_data.append(record[field])
     return field_data
 
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,6 @@
-from datetime import datetime, date
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from datetime import datetime, date
 # Runner script for all modules
 
 
@@ -10,28 +10,26 @@ from house_info import HouseInfo
 data = []
 print("Sensor Data App")
 
-time_fmt = '%m/%d/%y'
-
-data = load_sensor_data()
-print("Loaded records: {}".format(len(data)))
-
-
-house_info = HouseInfo(data)
-test_area = 1
-recs = house_info.get_data_by_area('id', test_area)
-print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
-
-test_date = datetime()
-
-test_date = datetime.strptime('5/9/20', time_fmt)
-recs = house_info.get_data_by_date('id', rec_date=test_date)
-print("\nHouse sensor records for date: {} = {}".format(test_date.strftime(time_fmt), len(recs)))
-
 ##############################
 
 # Module 1 code here:
 
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
+
 # Module 2 code here:
+
+
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area('id', rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
+
+test_date = datetime.strptime('5/9/20', '%m/%d/%y')
+recs = house_info.get_data_by_date('id', rec_date=test_date)
+print("\nHouse sensor records for date: {} = {}".format(test_date.strftime('%m/%d/%y'), len(recs)))
+
 
 # Module 3 code here:
 
